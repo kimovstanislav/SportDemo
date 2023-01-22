48fc6e4bb2aa2c2d7d54003be71fc90456773b08
skimov <kimovstanislav@gmail.com>
2023-01-22 15:56:13 +0100

    Created a project.

---
+++ SportDemo/SportDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportDemoApp.swift
+//  SportDemo
+//
+//  Created by Stanislav Kimov on 20.01.23.
+//
+
+import SwiftUI
+
+@main
+struct SportDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
