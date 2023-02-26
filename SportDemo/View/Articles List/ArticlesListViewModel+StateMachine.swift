3072fa98bff698c3d75442675a0738ae8455bdfa
skimov <kimovstanislav@gmail.com>
2023-02-26 16:53:13 +0100

    Refactor for a base state machine.

--- SportDemo/View/Articles List/ArticlesListViewModel+StateMachine.swift
+++ SportDemo/View/Articles List/ArticlesListViewModel+StateMachine.swift
@@ -8,9 +8,37 @@
 import Foundation
 import Combine
 
-// TODO: make a base class
+class BaseStateMachine<IState, IEvent> {
+    private(set) var state: IState {
+        didSet { stateSubject.send(self.state) }
+    }
+    private let stateSubject: PassthroughSubject<IState, Never>
+    let statePublisher: AnyPublisher<IState, Never>
+    
+    init(state: IState) {
+        self.state = state
+        self.stateSubject = PassthroughSubject<IState, Never>()
+        self.statePublisher = self.stateSubject.eraseToAnyPublisher()
+    }
+    
+    @discardableResult func tryEvent(_ event: IEvent) -> Bool {
+        guard let state = nextState(for: event) else {
+            return false
+        }
+        
+        self.state = state
+        return true
+    }
+    
+    // TODO: should be private, but can't make it overridable then.
+    func nextState(for event: IEvent) -> IState? {
+        fatalError("load(from:) has not been implemented")
+    }
+}
+
+
 extension ArticlesListViewModel {
-    class StateMachine {
+    class StateMachine: BaseStateMachine<StateMachine.State, StateMachine.Event> {
         enum State {
             case start
             case loadingArticles
@@ -24,59 +52,33 @@ extension ArticlesListViewModel {
             case loadArticlesFailure
         }
         
-        private(set) var state: State {
-            didSet { stateSubject.send(self.state) }
-        }
-        private let stateSubject: PassthroughSubject<State, Never>
-        let statePublisher: AnyPublisher<State, Never>
-        
-        init(state: State) {
-            self.state = state
-            self.stateSubject = PassthroughSubject<State, Never>()
-            self.statePublisher = self.stateSubject.eraseToAnyPublisher()
-        }
-    }
-}
-
-
-// MARK: - State changes
-
-extension ArticlesListViewModel.StateMachine {
-    @discardableResult func tryEvent(_ event: Event) -> Bool {
-        guard let state = nextState(for: event) else {
-            return false
-        }
-        
-        self.state = state
-        return true
-    }
-    
-    private func nextState(for event: Event) -> State? {
-        switch state {
-        case .start:
-            switch event {
+        override func nextState(for event: Event) -> State? {
+            switch state {
+            case .start:
+                switch event {
                 case .loadArticles: return .loadingArticles
-            default: break
-            }
-        case .loadingArticles:
-            switch event {
+                default: break
+                }
+            case .loadingArticles:
+                switch event {
                 case .loadArticlesSuccess: return .showArticles
                 case .loadArticlesFailure: return .error
-            default: break
-            }
-        case .showArticles:
-            switch event {
+                default: break
+                }
+            case .showArticles:
+                switch event {
                 case .loadArticles: return .loadingArticles
-            default: break
-            }
-        case .error:
-            switch event {
+                default: break
+                }
+            case .error:
+                switch event {
                 case .loadArticles: return .loadingArticles
-            default: break
+                default: break
+                }
             }
+            // Catch the wrong state
+            unexpectedCodePath(message: "Wrong event <\(event)> for state <\(state)>" )
         }
-        // Catch the wrong state
-        unexpectedCodePath(message: "Wrong event <\(event)> for state <\(state)>" )
     }
 }
 
